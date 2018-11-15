12092cc7c4296be2f0db0977459c5f06feb54ace
Siarhei Hatsuk <serhioalfa@mail.ru>
2018-11-15 21:27:37 +0300

    Many changes

--- pravo.php
+++ pravo.php
@@ -13,6 +13,8 @@ $headers = [
 	'AlexaToolbar-ALX_NS_PH: AlexaToolbar/alx-4.0.3'
 ];
 
+$searchRegion = ['Брестского', 'Жабинковского'];
+
 // Функция для отправки сообщения Вконтакте
 function vkMessage ($id, $message) {
 	$url = 'https://api.vk.com/method/messages.send';
@@ -34,77 +36,116 @@ function vkMessage ($id, $message) {
     )));
 }
 
-// Парсинг нужной нам страницы
-$ch = curl_init('http://www.pravo.by/ofitsialnoe-opublikovanie/novye-postupleniya/');
-	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-	curl_setopt($ch, CURLOPT_HEADER, true);
-	curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 5.2)');
-	curl_setopt($ch, CURLOPT_REFERER, 'https://www.yandex.by/');
-	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-	curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-	$result = curl_exec($ch);
-	curl_close($ch);
+if(date('N') <= 5) {
+	// Парсинг нужной нам страницы
+	$ch = curl_init('http://www.pravo.by/ofitsialnoe-opublikovanie/novye-postupleniya/');
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADER, true);
+		curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/5.0 (Windows; U; MSIE 7.0; Windows NT 5.2)');
+		curl_setopt($ch, CURLOPT_REFERER, 'http://www.pravo.by');
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+		curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+		$result = curl_exec($ch);
+		curl_close($ch);
 
-  	$dom = new simple_html_dom();
+	  	$dom = new simple_html_dom();
 
-  	$dom->load($result);
+	  	$dom->load($result);
 
-  	$all = $dom->find('.h-link__count b', 1);
+	  	$all = $dom->find('.h-link__count b', 1);
 
-  	$pravo = '<b>Всего ' .$all->plaintext. ' актов опубликовано ' .date('d.m.Y'). '</b>'; // Общее количество опубликованных актов
+	  	$pravo = '<b>Всего ' .$all->plaintext. ' актов опубликовано ' .date('d.m.Y'). '</b>'; // Общее количество опубликованных актов
 
-  	// Данные для smtp ('логин', 'пароль', 'хост', 'порт', 'кодировка письма')
-  	$mailSMTP = new SendMailSmtpClass('YOU MAIL HERE', 'PASSWORD HERE', 'ssl://smtp.gmail.com', 465, "UTF-8");
-	// От кого письмо
-	$from = array(
-	    "NAME", // Имя отправителя
-	    "YOU MAIL HERE" // Почта отправителя
-	);
-	// Кому письмо (через запятую указываем более одного ящика)
-	$to = 'you email here';
+	  	// Данные для smtp ('логин', 'пароль', 'хост', 'порт', 'кодировка письма')
+	  	$mailSMTP = new SendMailSmtpClass('YOU MAIL HERE', 'PASSWORD HERE', 'ssl://smtp.gmail.com', 465, "UTF-8");
+		// От кого письмо
+		$from = array(
+		    "NAME", // Имя отправителя
+		    "YOU MAIL HERE" // Почта отправителя
+		);
+		// Кому письмо (через запятую указываем более одного ящика)
+		$to = 'MAIL HERE';
 
-// Вытаскиваем нужную информацию
-foreach($dom->find('div.usercontent dl') as $dl) {
-	$text = trim($dl->plaintext);
-	preg_replace('/\s\s+/', ' ', $text);
-	foreach ($dl->find('a') as $link) {
-		$linkSend = "www.pravo.by" .$link->attr['href']; // Ссылка на документ
-		$messageTempVk = $text. "<br>" .$linkSend. "<br>";
-		$messageVk = trim(str_replace(chr(194).chr(160), ' ', html_entity_decode($messageTempVk))); // Текст для письма Вконтакте
-		$messageTempTel = preg_replace('/\s+/', ' ', $text). "\n" .$linkSend. "\n";
-		$messageTelegram = trim(str_replace(chr(194).chr(160), ' ', html_entity_decode($messageTempTel))); // Текст для письма в telegram
-		$message[] = $messageVk; // Массив с нужными данными
-		$messageT[] = $messageTelegram; // Массив с нужными данными для telegram
-	};
+	// Вытаскиваем нужную информацию
+	foreach($dom->find('div.usercontent dl') as $dl) {
+		$text = trim($dl->plaintext);
+		preg_replace('/\s\s+/', ' ', $text);
+		foreach ($dl->find('a') as $link) {
+			$linkSend = "www.pravo.by" .$link->attr['href']; // Ссылка на документ
+			$messageTempVk = $text. "<br>" .$linkSend. "<br>";
+			$messageVk = trim(str_replace(chr(194).chr(160), ' ', html_entity_decode($messageTempVk))); // Текст для письма Вконтакте
+			$messageTempTel = preg_replace('/\s+/', ' ', $text). "\n" .$linkSend. "\n";
+			$messageTelegram = trim(str_replace(chr(194).chr(160), ' ', html_entity_decode($messageTempTel))); // Текст для письма в telegram
+			$message[] = $messageVk; // Массив с нужными данными
+			$messageT[] = $messageTelegram; // Массив с нужными данными для telegram
+		};
+	}
+
+	// Цикл по массиву с нужными данными
+	foreach ($message as $wordSearch) {
+		switch($wordSearch[0]){ // Условия поиска по первому символу
+			case "1":
+			case "2": 
+			case "3": 
+			case "5": 
+				// Отправляем сообщение Вконтакте
+				vkMessage('ID VK', $wordSearch);
+				$mailMess[] = "<tr><td>" .$wordSearch. "</td></tr>"; // Записываем данные в массив для отправки на почту
+			}
+	}
+
+	// Поиск законов для конкретных регионов
+	foreach ($message as $region) {
+		switch($region[0]){ // Условия поиска по первому символу
+			case "9":
+			foreach ($searchRegion as $city) {
+				if (strpos($region, $city)) {
+	        		$mailRegion[] = "<tr><td>" .$region. "</td></tr>";
+	        		vkMessage('ID VK', $region);
+	        	}
+	        	else {
+	        		false;
+	        	}
+	        }
+    	}	
+	}
+
+	// Отправляем одно письмо на электронную почту (кому, тема сообщения, текст, куда)
+	$mailSMTP->send($to, 'Законы от ' .date('d.m.Y'), '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"><h1>Список опубликованных законов по фильтру:</h1><table>' .implode('', $mailMess).'<h2>Региональные законы:</h2>' .implode('', $mailRegion). '</table><h3>' .$pravo. '</h3>', $from);
+
+	// Цикл по массиву с нужными данными для Telegram
+	foreach ($messageT as $word) {
+		switch($word[0]){ // Условия поиска по первому символу
+			case "1":
+			case "2":
+			case "3":
+			case "5":
+				// Отправляем сообщение в telegram
+				file_get_contents('https://api.telegram.org/TOKENHERE/sendMessage?chat_id=YOUCHATIDHERE&disable_web_page_preview=false&parse_mode=html&text='.urlencode($word));
+			}
+	}
+
+	file_get_contents('https://api.telegram.org/TOKENHERE/sendMessage?chat_id=YOUCHATIDHERE&disable_web_page_preview=false&parse_mode=html&text=<b>Региональные законы:</b>');
+
+	// Поиск законов для конкретных регионов для Telegram
+	foreach ($messageT as $regionT) {
+		switch($regionT[0]){ // Условия поиска по первому символу
+			case "9":
+			foreach ($searchRegion as $city) {
+				if (strpos($regionT, $city)) {
+	        		file_get_contents('https://api.telegram.org/TOKENHERE/sendMessage?chat_id=YOUCHATIDHERE&disable_web_page_preview=false&parse_mode=html&text='.urlencode($regionT));
+	        	}
+	        	else {
+	        		false;
+	        	}
+	        }
+    	}
+	}
+
+	file_get_contents('https://api.telegram.org/TOKENHERE/sendMessage?chat_id=YOUCHATIDHERE&disable_web_page_preview=false&parse_mode=html&text='.urlencode($pravo));
 }
-
-// Цикл по массиву с нужными данными
-foreach ($message as $wordSearch) {
-	switch($wordSearch[0]){ // Условия поиска по первому символу
-		case "1":
-		case "2": 
-		case "3": 
-		case "5": 
-			// Отправляем сообщение Вконтакте
-			// id101487135
-			vkMessage('you id vk', $wordSearch);
-			$mailMess[] = "<tr><td>" .$wordSearch. "</td></tr>"; // Записываем данные в массив для отправки на почту
-		}
-}
-
-// Отправляем одно письмо на электронную почту (кому, тема сообщения, текст, куда)
-$mailSMTP->send($to, 'Законы от ' .date('d.m.Y'), '<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"><table>' .implode('', $mailMess). '</table>' .$pravo, $from);
-
-// Цикл по массиву с нужными данными для telegram
-foreach ($messageT as $word) {
-	switch($word[0]){ // Условия поиска по первому символу
-		case "1":
-		case "2":
-		case "3":
-		case "5":
-			// Отправляем сообщение в telegram
-			file_get_contents('https://api.telegram.org/TOKENHERE/sendMessage?chat_id=YOUCHATIDHERE&disable_web_page_preview=false&parse_mode=html&text='. urlencode($word));
-		}
+else {
+	false;
 }
 ?>
